41fb2203aad5a9dba0ba5a90b0c91ac1631da665
Kirill Glebov <kg891277@broadcom.net>
2019-07-26 17:12:06 +0200

    Wip

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { HeroService} from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
@@ -20,9 +24,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
